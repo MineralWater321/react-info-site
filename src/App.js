0caaa2642b01069c3445933f88b865a2873c09a5
Edward Paler <paler.edward@gmail.com>
2022-02-02 00:06:36 +0800

    divide components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-
 import './App.css';
 import logo from './react-logo.png'
+
 function App() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src={logo} width='40px' />
-        </nav>
-      </header>
-      <h1>Reasons I love React</h1>
-      <ol>
-        <li>I love React</li>
-        <li>It's good for beginners</li>
-      </ol>
-      <footer>
-        <small>
-          2020 Paler development. All rights reserved.
-        </small>
-      </footer>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
 
+function Header() {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <img src={logo} className="img-logo" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+function MainContent() {
+  return (
+    <div>
+      <h1>Reasons I want to learn React</h1>
+      <ol>
+        <li>I love React</li>
+        <li>It's good for beginners</li>
+      </ol>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>
+        2020 Paler development. All rights reserved.
+      </small>
+    </footer>
+  )
+}
 export default App;
